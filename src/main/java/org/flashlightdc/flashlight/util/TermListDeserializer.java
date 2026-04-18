ac9c165b27ad91bdb1e3ae4c60ad8a577a558506
bhaditya-cs <bhadraaditya03@gmail.com>
2026-04-18 18:23:26 -0500

    db connection - added dependencies and more config in application.properties; wallet is now required to run application

--- src/main/java/org/flashlightdc/flashlight/util/TermListDeserializer.java
+++ src/main/java/org/flashlightdc/flashlight/util/TermListDeserializer.java
@@ -1,19 +1,20 @@
 package org.flashlightdc.flashlight.util;
 
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import org.flashlightdc.flashlight.dto.TermDto;
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ValueDeserializer;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TermListDeserializer extends ValueDeserializer<List<TermDto>> {
+public class TermListDeserializer extends JsonDeserializer<List<TermDto>> {
 
     @Override
-    public List<TermDto> deserialize(JsonParser p, DeserializationContext ctx) throws JacksonException {
+    public List<TermDto> deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
         System.out.println("Verify this ran");
         JsonNode node = ctx.readTree(p);
         JsonNode items = node.get("item");
