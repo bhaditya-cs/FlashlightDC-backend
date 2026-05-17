4324f08ed67f3fad2438e048e0d11369b55ed3c2
bhaditya-cs <bhadraaditya03@gmail.com>
2026-05-16 23:27:27 -0500

    docker compose, ci/cd

--- src/main/java/org/flashlightdc/flashlight/config/CorsConfig.java
+++ src/main/java/org/flashlightdc/flashlight/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(frontendUrl)
+                .allowedOrigins("*")
                 .allowedMethods("GET")
                 .maxAge(3600);
     }
